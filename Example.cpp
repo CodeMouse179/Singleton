9bbe7e052694d447b442845169a782cae5ca6267
CodeMouse179 <codemouse179@gmail.com>
2022-11-23 23:03:21 +0300

    add first impl

--- Example.cpp
+++ Example.cpp
@@ -1,6 +1,15 @@
 ﻿#include "Singleton.hpp"
 
+class TestClass : public CodeMouse::Singleton<TestClass>
+{
+public:
+    TestClass()
+    {
+    }
+};
+
 int main()
 {
+    TestClass instance = TestClass::GetInstance();
     return 0;
 }
--- Singleton.hpp
+++ Singleton.hpp
@@ -1,1 +1,24 @@
-﻿
+﻿#ifndef CODEMOUSE_SINGLETON_HPP
+#define CODEMOUSE_SINGLETON_HPP
+
+namespace CodeMouse
+{
+    //Generic Singleton <static class>
+    template<typename T>
+    class Singleton
+    {
+    protected: //Allow subclasses to access the constructor.
+        Singleton()
+        {
+        }
+
+    public:
+        static T& GetInstance()
+        {
+            static T instance;
+            return instance;
+        }
+    };
+}
+
+#endif
