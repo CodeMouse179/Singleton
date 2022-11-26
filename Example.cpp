c2faabc27699d5a012ba41e9dd65af7facd7ea54
CodeMouse179 <codemouse179@gmail.com>
2022-11-26 17:08:24 +0300

    release 1.0

--- Example.cpp
+++ Example.cpp
@@ -16,9 +16,22 @@ public:
     }
 };
 
+class NormalClass
+{
+public:
+    NormalClass()
+    {
+    }
+};
+
 int main()
 {
+    //Usage 1 (Inherit class CodeMouse::Singleton):
     TestClass instance = TestClass::GetInstance();
     a instance_a = a::GetInstance();
+
+    //Usage 2 (Generic static function):
+    NormalClass instance2 = CodeMouse::Singleton<NormalClass>::GetInstance();
+
     return 0;
 }
--- Singleton.hpp
+++ Singleton.hpp
@@ -1,4 +1,18 @@
-﻿#ifndef CODEMOUSE_SINGLETON_HPP
+﻿//     +--------------------------------------------------------------------------------+
+//     |                                 Singleton v1.0.0                               |
+//     |  Introduction : Generic Singleton for C++                                      |
+//     |  Modified Date : 2022/11/26                                                    |
+//     |  License : MIT                                                                 |
+//     |  Source Code : https://github.com/CodeMouse179/Singleton                       |
+//     |  Readme : https://github.com/CodeMouse179/Singleton/blob/main/README.md        |
+//     |                                                                                |
+//     |                             Designer : CodeMouse179                            |
+//     |  Email : codemouse179@gmail.com                                                |
+//     |  Github : https://github.com/CodeMouse179                                      |
+//     |  Bilibili : https://space.bilibili.com/3461577785215838                        |
+//     +--------------------------------------------------------------------------------+
+
+#ifndef CODEMOUSE_SINGLETON_HPP
 #define CODEMOUSE_SINGLETON_HPP
 
 #define SINGLETON_CLASS(class_name) class class_name : public CodeMouse::Singleton<class_name>
